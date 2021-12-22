6777eebfc6b495e585dccea9b2721a0f2a7c86f7
Pep Llaneras <1679807+jllaneras@users.noreply.github.com>
2021-12-22 23:43:41 +0100

    Increase window size

--- gtk3demo/MainWindow.py
+++ gtk3demo/MainWindow.py
@@ -8,6 +8,8 @@ class MainWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Hello ProtonMail!")
 
+        self.set_size_request(400, 600)
+
         self.button = Gtk.Button(label="Connect")
         self.button.connect("clicked", self.on_button_clicked)
         self.add(self.button)
